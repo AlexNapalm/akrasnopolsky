eebb863b2b8950a66264801f04fbe13470949052
AlexNapalm <aak91@mail.ru>
2017-07-29 19:52:24 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * / ^.
+ * @author akrasnopolsky
+ * @since 29.07.2017
+ * @version 1
+ */
 public class Calculate{
+
+/**
+ * Main
+ * @param args 
+ * @return Метод ничего не возвращает
+ */ 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
