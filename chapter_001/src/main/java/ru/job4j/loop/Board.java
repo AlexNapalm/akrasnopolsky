50f101aef71bcca1d0140cd2409cbb073efb4032
AlexNapalm <aak91@mail.ru>
2017-08-09 17:22:07 +0300

    4.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * Класс, содержащий метод для построения шахматной доски.
+ * 4.3. Построить шахматную доску в псевдографике. [#13559]
+ */
+public class Board {
+    public static void main(String[] args) {
+        System.out.println(paint(5, 4));
+        System.out.printf("end");
+    }
+
+    /**
+     * Метод для построения шахматной доски в псевдографике.
+     * @param width ширина доски.
+     * @param height высота доски.
+     * @return строка символов Х и пробелов в виде шахматной доски.
+     */
+    public static String paint(int width, int height) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                builder.append((j + i) % 2 == 0 ? "x" : " ");
+            }
+            builder.append(System.lineSeparator());
+        }
+        return builder.toString();
+    }
+}
