67c5e3041198b993e0091ae08f6a2926fd19aff9
AlexNapalm <aak91@mail.ru>
2017-12-10 22:13:18 +0300

    5.3.2. Создать контейнер на базе связанного списка  [#159]

---
+++ chapter_005/src/main/java/ru/job4j/list/LinkedContainer.java
@@ -0,0 +1,160 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedContainer<E> implements Iterable<E> {
+    /**
+     * Start of the list.
+     */
+    private Node<E> firstNode;
+    /**
+     * End of the list.
+     */
+    private Node<E> lastNode;
+    /**
+     * Size of the container.
+     */
+    private int size = 0;
+
+    /**
+     * Constructs LinkedContainer.
+     */
+    public LinkedContainer() {
+        lastNode = new Node<E>(null, firstNode, null);
+        firstNode = new Node<E>(null, null, lastNode);
+    }
+    /**
+     * Adds element to the end of the list.
+     * @param e element to add.
+     */
+    public void addLast(E e) {
+        Node<E> prev = lastNode;
+        prev.setCurrentElement(e);
+        lastNode = new Node<E>(null, prev, null);
+        prev.setNextElement(lastNode);
+        size++;
+    }
+    /**
+     * Adds element to the beginning of the list.
+     * @param e
+     */
+    public void addFirst(E e) {
+        Node<E> next = firstNode;
+        next.setCurrentElement(e);
+        firstNode = new Node<E>(null, null, next);
+        next.setPrevElement(firstNode);
+        size++;
+
+    }
+
+    /**
+     * Return the actual size of the list.
+     * @return size.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Return the element by the index.
+     * @param index index.
+     * @return element.
+     */
+    public E getElementByIndex(int index) {
+        Node<E> target = firstNode.getNextElement();
+        for (int i = 0; i < index; i++) {
+            target = getNextElement(target);
+        }
+        return target.getCurrentElement();
+    }
+
+    private Node<E> getNextElement(Node<E> current) {
+        return current.getNextElement();
+    }
+
+    /**
+     * Iterator from the first to last.
+     * @return
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                return position < size;
+            }
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    return getElementByIndex(position++);
+                } else {
+                    throw  new NoSuchElementException();
+                }
+            }
+        };
+    }
+
+    /**
+     * Iterator from last to the first.
+     * @return
+     */
+    public Iterator<E> descendingIterator() {
+        return new Iterator<E>() {
+            int position = size - 1;
+
+            @Override
+            public boolean hasNext() {
+                return position >= 0;
+            }
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    return getElementByIndex(position--);
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+
+    private class Node<E> {
+        private E currentElement;
+        private Node<E> nextElement;
+        private Node<E> prevElement;
+
+        private Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
+            this.currentElement = currentElement;
+            this.prevElement = prevElement;
+            this.nextElement = nextElement;
+        }
+
+        public E getCurrentElement() {
+            return currentElement;
+        }
+
+        public void setCurrentElement(E currentElement) {
+            this.currentElement = currentElement;
+        }
+
+        public Node<E> getNextElement() {
+            return nextElement;
+        }
+
+        public void setNextElement(Node<E> nextElement) {
+            this.nextElement = nextElement;
+        }
+
+        public Node<E> getPrevElement() {
+            return prevElement;
+        }
+
+        public void setPrevElement(Node<E> prevElement) {
+            this.prevElement = prevElement;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/list/LinkedContainerTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class LinkedContainerTest {
+
+    LinkedContainer<String> lc = new LinkedContainer<>();
+
+    @Before
+    public void setUp() {
+        lc.addLast("one");
+        lc.addLast("two");
+        lc.addLast("three");
+        lc.addLast("four");
+        lc.addLast("five");
+    }
+
+    @Test
+    public void whenAddLastThenAddedInTheEnd() {
+        lc.addLast("TEST");
+
+        assertThat(lc.getElementByIndex(5), is("TEST"));
+    }
+
+    @Test
+    public void whenAddFisrtThenAddedInTheBegin() {
+        lc.addFirst("TEST");
+
+        assertThat(lc.getElementByIndex(0), is("TEST"));
+    }
+
+    @Test
+    public void whenListFilledThenReturnSize() {
+        assertThat(lc.size(), is(5));
+
+        lc.addFirst("TEST");
+
+        assertThat(lc.size(), is(6));
+    }
+
+    @Test
+    public void whenSearchByIndexThenReturnRightItem() {
+        assertThat(lc.getElementByIndex(0), is("one"));
+        assertThat(lc.getElementByIndex(1), is("two"));
+        assertThat(lc.getElementByIndex(2), is("three"));
+        assertThat(lc.getElementByIndex(3), is("four"));
+        assertThat(lc.getElementByIndex(4), is("five"));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void iterator() {
+        Iterator iterator = lc.iterator();
+
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("one"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("two"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("three"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("four"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("five"));
+        assertThat(iterator.hasNext(), is(false));
+        iterator.next();
+
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void descendingIterator() {
+        Iterator iterator = lc.descendingIterator();
+
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("five"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("four"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("three"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("two"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("one"));
+        assertThat(iterator.hasNext(), is(false));
+        iterator.next();
+    }
+}
