922eb917c1caf64f281fa069a61cd00b8603f7d5
AlexNapalm <aak91@mail.ru>
2018-01-29 20:47:06 +0300

    6.3.3. ThreadSafe динамический список  [#1105]

--- chapter_005/src/main/java/ru/job4j/list/DynamicList.java
+++ chapter_005/src/main/java/ru/job4j/list/DynamicList.java
@@ -1,17 +1,27 @@
 package ru.job4j.list;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
+@ThreadSafe
 public class DynamicList<E> implements Iterable<E> {
+    /**
+     * Lock object for sync.
+     */
+    private final Object lock;
+
     /**
      * Array of elements.
      */
+    @GuardedBy("lock")
     private Object[] container;
     /**
      * Actual filled size of container.
      */
+    @GuardedBy("lock")
     private int size = 0;
     /**
      * Constant for default capacity.
@@ -27,6 +37,7 @@ public class DynamicList<E> implements Iterable<E> {
      */
     public DynamicList() {
         this.container = new Object[DEFAULT_CAPACITY];
+        this.lock = new Object();
     }
 
     /**
@@ -35,62 +46,80 @@ public class DynamicList<E> implements Iterable<E> {
      */
     public DynamicList(int size) {
         this.container = new Object[size];
+        this.lock = new Object();
     }
 
     /**
      * Adds an element.
      * @param value element to add.
      */
+    @GuardedBy("lock")
     public void add(E value) {
-        if (size >= this.container.length) {
-            grow();
+        synchronized (this.lock) {
+            if (size >= this.container.length) {
+                grow();
+            }
+            this.container[size++] = value;
         }
-        this.container[size++] = value;
     }
-
     /**
      * Returns element by index.
      * @param index index.
      * @return element.
      */
+    @GuardedBy("lock")
     public E get(int index) {
-        return (E) this.container[index];
+        synchronized (this.lock) {
+            return (E) this.container[index];
+        }
     }
 
     /**
      * Return size of the container;
      * @return size.
      */
+    @GuardedBy("lock")
     public int size() {
-        return this.container.length;
+        synchronized (this.lock) {
+            return this.container.length;
+        }
     }
 
     /**
      * Grows the size of container if there is no space to store elements.
      */
+    @GuardedBy("lock")
     private void grow() {
-        int oldCapacity = this.container.length;
-        int newCapacity = oldCapacity + GROW_STEP;
-        this.container = Arrays.copyOf(this.container, newCapacity);
+        synchronized (this.lock) {
+            int oldCapacity = this.container.length;
+            int newCapacity = oldCapacity + GROW_STEP;
+            this.container = Arrays.copyOf(this.container, newCapacity);
+        }
     }
 
-
+    @GuardedBy("lock")
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
+            @GuardedBy("lock")
             int position = 0;
 
+            @GuardedBy("lock")
             @Override
             public boolean hasNext() {
-                return position < size;
+                synchronized (lock) {
+                    return position < size;
+                }
             }
-
+            @GuardedBy("lock")
             @Override
             public E next() {
-                if (hasNext()) {
-                    return (E) container[position++];
-                } else {
-                    throw new NoSuchElementException();
+                synchronized (lock) {
+                    if (hasNext()) {
+                        return (E) container[position++];
+                    } else {
+                        throw new NoSuchElementException();
+                    }
                 }
             }
         };
--- chapter_005/src/main/java/ru/job4j/list/LinkedContainer.java
+++ chapter_005/src/main/java/ru/job4j/list/LinkedContainer.java
@@ -1,20 +1,31 @@
 package ru.job4j.list;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
+@ThreadSafe
 public class LinkedContainer<E> implements Iterable<E> {
+    /**
+     * Lock object for sync.
+     */
+    private final Object lock;
+
     /**
      * Start of the list.
      */
+    @GuardedBy("lock")
     private Node<E> firstNode = new Node<E>(null, null, null);
     /**
      * End of the list.
      */
+    @GuardedBy("lock")
     private Node<E> lastNode = new Node<E>(null, null, null);
     /**
      * Size of the container.
      */
+    @GuardedBy("lock")
     private int size = 0;
 
     /**
@@ -23,61 +34,76 @@ public class LinkedContainer<E> implements Iterable<E> {
     public LinkedContainer() {
         firstNode.setNextElement(lastNode);
         lastNode.setPrevElement(firstNode);
+        this.lock = new Object();
     }
     /**
      * Adds element to the end of the list.
      * @param e element to add.
      */
+    @GuardedBy("lock")
     public void addLast(E e) {
-        Node<E> prev = lastNode;
-        prev.setCurrentElement(e);
-        lastNode = new Node<E>(null, prev, null);
-        prev.setNextElement(lastNode);
-        size++;
+        synchronized (this.lock) {
+            Node<E> prev = lastNode;
+            prev.setCurrentElement(e);
+            lastNode = new Node<E>(null, prev, null);
+            prev.setNextElement(lastNode);
+            size++;
+        }
     }
     /**
      * Adds element to the beginning of the list.
      * @param e
      */
+    @GuardedBy("lock")
     public void addFirst(E e) {
-        Node<E> next = firstNode;
-        next.setCurrentElement(e);
-        firstNode = new Node<E>(null, null, next);
-        next.setPrevElement(firstNode);
-        size++;
-
+        synchronized (this.lock) {
+            Node<E> next = firstNode;
+            next.setCurrentElement(e);
+            firstNode = new Node<E>(null, null, next);
+            next.setPrevElement(firstNode);
+            size++;
+        }
     }
 
     /**
      * Removes last element from container.
      */
+    @GuardedBy("lock")
     public void removeLast() {
-        Node<E> last = lastNode.getPrevElement();
-        Node<E> newLast = last.getPrevElement();
-        last.setCurrentElement(null);
-        newLast.setNextElement(lastNode);
-        lastNode.setPrevElement(newLast);
-        size--;
+        synchronized (this.lock) {
+            Node<E> last = lastNode.getPrevElement();
+            Node<E> newLast = last.getPrevElement();
+            last.setCurrentElement(null);
+            newLast.setNextElement(lastNode);
+            lastNode.setPrevElement(newLast);
+            size--;
+        }
     }
 
     /**
      * Removes the first element from container.
      */
+    @GuardedBy("lock")
     public void removeFirst() {
-        Node<E> first = firstNode.getNextElement();
-        Node<E> newFirst = first.getNextElement();
-        first.setCurrentElement(null);
-        newFirst.setPrevElement(firstNode);
-        firstNode.setNextElement(newFirst);
-        size--;
+        synchronized (this.lock) {
+            Node<E> first = firstNode.getNextElement();
+            Node<E> newFirst = first.getNextElement();
+            first.setCurrentElement(null);
+            newFirst.setPrevElement(firstNode);
+            firstNode.setNextElement(newFirst);
+            size--;
+        }
     }
 
     /**
      * Return the actual size of the list.
      * @return size.
      */
+    @GuardedBy("lock")
     public int size() {
-        return size;
+        synchronized (this.lock) {
+            return size;
+        }
     }
 
     /**
@@ -85,12 +111,15 @@ public class LinkedContainer<E> implements Iterable<E> {
      * @param index index.
      * @return element.
      */
+    @GuardedBy("lock")
     public E getElementByIndex(int index) {
-        Node<E> target = firstNode.getNextElement();
-        for (int i = 0; i < index; i++) {
-            target = getNextElement(target);
+        synchronized (this.lock) {
+            Node<E> target = firstNode.getNextElement();
+            for (int i = 0; i < index; i++) {
+                target = getNextElement(target);
+            }
+            return target.getCurrentElement();
         }
-        return target.getCurrentElement();
     }
 
     private Node<E> getNextElement(Node<E> current) {
@@ -101,24 +130,32 @@ public class LinkedContainer<E> implements Iterable<E> {
      * Iterator from the first to last.
      * @return
      */
+    @GuardedBy("lock")
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
+            @GuardedBy("lock")
             Node<E> element = firstNode.getNextElement();
 
+            @GuardedBy("lock")
             @Override
             public boolean hasNext() {
-                return element != lastNode;
+                synchronized (lock) {
+                    return element != lastNode;
+                }
             }
 
+            @GuardedBy("lock")
             @Override
             public E next() {
-                if (hasNext()) {
-                    E result = element.currentElement;
-                    element = element.nextElement;
-                    return result;
-                } else {
-                    throw  new NoSuchElementException();
+                synchronized (lock) {
+                    if (hasNext()) {
+                        E result = element.currentElement;
+                        element = element.nextElement;
+                        return result;
+                    } else {
+                        throw  new NoSuchElementException();
+                    }
                 }
             }
         };
@@ -128,64 +165,91 @@ public class LinkedContainer<E> implements Iterable<E> {
      * Iterator from last to the first.
      * @return
      */
+    @GuardedBy("lock")
     public Iterator<E> descendingIterator() {
         return new Iterator<E>() {
+            @GuardedBy("lock")
             int position = size - 1;
+            @GuardedBy("lock")
             Node<E> element = lastNode.getPrevElement();
+            @GuardedBy("lock")
             Node<E> prev = lastNode.getPrevElement();
 
+            @GuardedBy("lock")
             @Override
             public boolean hasNext() {
-                return position >= 0;
+                synchronized (lock) {
+                    return position >= 0;
+                }
             }
 
+            @GuardedBy("lock")
             @Override
             public E next() {
-                if (hasNext()) {
-                    element = prev;
-                    prev = prev.getPrevElement();
-                    position--;
-                    return element.getCurrentElement();
-                } else {
-                    throw new NoSuchElementException();
+                synchronized (lock) {
+                    if (hasNext()) {
+                        element = prev;
+                        prev = prev.getPrevElement();
+                        position--;
+                        return element.getCurrentElement();
+                    } else {
+                        throw new NoSuchElementException();
+                    }
                 }
             }
         };
     }
-
+    @ThreadSafe
     private class Node<E> {
+        private final Object lock;
+        @GuardedBy("lock")
         private E currentElement;
+        @GuardedBy("lock")
         private Node<E> nextElement;
+        @GuardedBy("lock")
         private Node<E> prevElement;
 
         private Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
             this.currentElement = currentElement;
             this.prevElement = prevElement;
             this.nextElement = nextElement;
+            this.lock = new Object();
         }
-
+        @GuardedBy("lock")
         public E getCurrentElement() {
-            return currentElement;
+            synchronized (this.lock) {
+                return currentElement;
+            }
         }
-
+        @GuardedBy("lock")
         public void setCurrentElement(E currentElement) {
-            this.currentElement = currentElement;
+            synchronized (this.lock) {
+                this.currentElement = currentElement;
+            }
         }
-
+        @GuardedBy("lock")
         public Node<E> getNextElement() {
-            return nextElement;
+            synchronized (this.lock) {
+                return nextElement;
+            }
         }
-
+        @GuardedBy("lock")
         public void setNextElement(Node<E> nextElement) {
-            this.nextElement = nextElement;
+            synchronized (this.lock) {
+                this.nextElement = nextElement;
+            }
         }
-
+        @GuardedBy("lock")
         public Node<E> getPrevElement() {
-            return prevElement;
+            synchronized (this.lock) {
+                return prevElement;
+            }
         }
-
+        @GuardedBy("lock")
         public void setPrevElement(Node<E> prevElement) {
-            this.prevElement = prevElement;
+            synchronized (this.lock) {
+                this.prevElement = prevElement;
+            }
         }
     }
 }
