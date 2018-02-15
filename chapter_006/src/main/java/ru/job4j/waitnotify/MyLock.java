8e5af1468142cdcc5f44fe5576f73f7ae06523c2
AlexNapalm <aak91@mail.ru>
2018-02-15 20:15:14 +0300

    6.4.3. Реализовать собственный механизм блокировок Lock.  [#1100]

--- chapter_006/src/main/java/ru/job4j/waitnotify/MyLock.java
+++ chapter_006/src/main/java/ru/job4j/waitnotify/MyLock.java
@@ -1,90 +1,40 @@
 package ru.job4j.waitnotify;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-
-public class MyLock implements Lock {
+public class MyLock {
 
     private final Object lock = new Object();
-
     private boolean isLocked = false;
+    private Thread lockedBy = null;
+    private int lockCount = 0;
 
-    private Thread thread = null;
-
-    @Override
     public void lock() {
         synchronized (this.lock) {
-            while (this.isLocked) {
+            Thread callingThread = Thread.currentThread();
+
+            while (isLocked && lockedBy != callingThread) {
                 try {
-                    this.wait();
+                    this.lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             this.isLocked = true;
-            this.thread = Thread.currentThread();
+            this.lockedBy = Thread.currentThread();
+            lockCount++;
         }
     }
 
-    @Override
-    public void lockInterruptibly() throws InterruptedException {
-        if (Thread.interrupted()) {
-            throw new InterruptedException();
-        }
-        synchronized (this.lock) {
-            if (this.thread != null) {
-                this.thread.interrupt();
-                this.isLocked = false;
-                this.notifyAll();
-            }
-        }
-    }
-
-    @Override
-    public synchronized boolean tryLock() {
-        boolean lock = false;
-        synchronized (this.lock) {
-            if (!this.isLocked) {
-                this.isLocked = true;
-                lock = true;
-                this.thread = Thread.currentThread();
-            }
-        }
-        return lock;
-    }
-
-    @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        if (Thread.interrupted()) {
-            throw new InterruptedException();
-        }
-        boolean lock = false;
-        long waitTime = System.currentTimeMillis() + unit.toMillis(time);
-        synchronized (this.lock) {
-            while (this.isLocked && waitTime > System.currentTimeMillis()) {
-                this.lock.wait(waitTime - System.currentTimeMillis());
-            }
-            if (!this.isLocked) {
-                this.isLocked = true;
-                lock = true;
-                this.thread = Thread.currentThread();
-            }
-        }
-        return lock;
-    }
-
-    @Override
     public void unlock() {
         synchronized (this.lock) {
-            this.isLocked = false;
-        }
-        this.thread = null;
-        this.notifyAll();
-    }
+            if (Thread.currentThread() == lockedBy) {
+                lockCount--;
 
-    @Override
-    public Condition newCondition() {
-        throw new UnsupportedOperationException();
+                if (lockCount == 0) {
+                    isLocked = false;
+                    lockedBy = null;
+                    notifyAll();
+                }
+            }
+        }
     }
 }
